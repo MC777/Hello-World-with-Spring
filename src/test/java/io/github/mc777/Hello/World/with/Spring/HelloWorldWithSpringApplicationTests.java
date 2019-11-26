855759be417eeb84494e563064f8a13598ef13bb
Marek Ciesla <ciesla.marek@gmail.com>
2019-11-27 00:02:11 +0100

    initial state from start.spring.io

---
+++ src/test/java/io/github/mc777/Hello/World/with/Spring/HelloWorldWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mc777.Hello.World.with.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
