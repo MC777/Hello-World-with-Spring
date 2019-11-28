575942422a02c92602c1fe20e571ca1383bba1be
Marek Ciesla <ciesla.marek@gmail.com>
2019-11-29 00:06:25 +0100

    clean: spring boot test reformat

--- src/test/java/io/github/mc777/Hello/World/with/Spring/HelloWorldWithSpringApplicationTests.java
+++ src/test/java/io/github/mc777/Hello/World/with/Spring/HelloWorldWithSpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HelloWorldWithSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
